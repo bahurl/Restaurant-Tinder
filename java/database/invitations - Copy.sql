7fa0b52c1eefd1002bb998dd7c370856bc33838d
Ricardo Ramos <ricardo.r.p466@gmail.com>
2022-11-07 10:30:34 -0500

    restaurant tables

--- java/database/invitations - Copy.sql
+++ java/database/invitations - Copy.sql
@@ -1,42 +1,14 @@
-BEGIN TRANSACTION;
 DROP TABLE IF EXISTS invitations;
 DROP TABLE IF EXISTS restaurants;
 DROP TABLE IF EXISTS days;
 DROP TABLE IF EXISTS restaurant_days;
 DROP TABLE IF EXISTS thumbs_up;
 
-DROP SEQUENCE IF EXISTS seq_invitation_id;
-DROP SEQUENCE IF EXISTS seq_restaurant_id;
-DROP SEQUENCE IF EXISTS seq_day_id;
-DROP SEQUENCE IF EXISTS seq_thumbs_up_id;
 
-CREATE SEQUENCE seq_invitation_id
-  INCREMENT BY 1
-  NO MAXVALUE
-  NO MINVALUE
-  CACHE 1;
-
-CREATE SEQUENCE seq_restaurant_id
-  INCREMENT BY 1
-  NO MAXVALUE
-  NO MINVALUE
-  CACHE 1;
-
-CREATE SEQUENCE seq_day_id
-  INCREMENT BY 1
-  NO MAXVALUE
-  NO MINVALUE
-  CACHE 1;
-
-CREATE SEQUENCE seq_thumbs_up_id
-  INCREMENT BY 1
-  NO MAXVALUE
-  NO MINVALUE
-  CACHE 1;
 /***************************************/
 /***************************************/
-CREATE TABLE invitations (
-invitation_id int DEFAULT nextval('seq_invitation_id'::regclass) NOT NULL,
+CREATE TABLE invitations(
+invitation_id serial NOT NULL,
 owner_id int NOT NULL,
 city varchar(200),
 zip_code varchar(5),
@@ -47,12 +19,12 @@ CONSTRAINT PK_invitation_id PRIMARY KEY (invitation_id)
 /***************************************/
 /***************************************/
 CREATE TABLE restaurants(
-restaurant_id int DEFAULT nextval('seq_restaurant_id'::regclass) NOT NULL,
+restaurant_id serial NOT NULL,
 name varchar(200) NOT NULL,
 type varchar(50) NOT NULL,
 address1 varchar(200) NOT NULL,
-address2 varchar(200) NOT NULL,
-address3 varchar(200) NOT NULL,
+address2 varchar(200),
+address3 varchar(200),
 city varchar(100) NOT NULL,
 state varchar(50) NOT NULL,
 zip_code varchar(5) NOT NULL,
@@ -64,23 +36,23 @@ CONSTRAINT PK_restaurant_id PRIMARY KEY (restaurant_id)
 /***************************************/
 /***************************************/
 CREATE TABLE days(
-day_id int DEFAULT nextval('seq_day_id'::regclass) NOT NULL,
+day_id serial NOT NULL,
 day_name varchar(20),
-CONSTRAINT PK_days_id PRIMARY KEY (day_id)
+CONSTRAINT PK_day_id PRIMARY KEY (day_id)
 );
 /***************************************/
 /***************************************/
 CREATE TABLE restaurant_days(
 restaurant_id int NOT NULL,
 day_id int NOT NULL,
-CONSTRAINT PK_restaurant_days PRIMARY KEY (restaurant_id,days_id),
+CONSTRAINT PK_restaurant_days PRIMARY KEY (restaurant_id,day_id),
 CONSTRAINT FK_restaurant_id FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id),
 CONSTRAINT FK_day_id FOREIGN KEY (day_id) REFERENCES days(day_id)
 );
 /***************************************/
 /***************************************/
 CREATE TABLE thumbs_up(
-thumbs_up_id int DEFAULT nextval('seq_thumbs_up_id'::regclass) NOT NULL,
+thumbs_up_id serial NOT NULL,
 invitation_id int NOT NULL,
 restaurant_id int NOT NULL,
 CONSTRAINT PK_thumbs_up_id PRIMARY KEY (thumbs_up_id),
@@ -107,4 +79,13 @@ VALUES
 INSERT INTO restaurants(name,type,address1,city,state,zip_code,phone,open_hour,close_hour)
 VALUES
 ('Ginger Dim Sum','Dim Sum','1912 Kings Hwy','Port Charlotte','FL','33980','9413915768','8:30','21:00'),
-('Tavola','Pizza','488 9th Ave', 'New York','NY','10018')
+('Tavola','Pizza','488 9th Ave', 'New York','NY','10018','7304468945','9:00','19:00'),
+('Baskin Robins','Burger','123 Main Ave','Port Charlotte','FL','33980','9413911150','11:00','22:00'),
+('Mint Leaf','Thai','1456 South Ave','Port Charlotte','FL','33980','9413915587','11:00','16:00'),
+('Chinos','Ramen','982 5th Ave','New York','NY','10018','7304468952','16:00','02:00'),
+('Rickys','Colombian','359 12th Ave','New York','NY','10018','7304468957','07:00','17:00'),
+('Late Night Study','Chinese','1568 Highland Ave','Port Charlotte','FL','33980','9149154682','17:00','03:00'),
+('Brekkie','Egg','125 3rd Ave','New York','NY','10018','7304469857','06:00','14:00');
+
+
+
